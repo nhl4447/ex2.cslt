bb4c2f839f14af70febde2ec7adb3129cab97a05
Lam Nguyen <pronhl4447@gmail.com>
2026-05-01 22:02:33 +0700

    Add project files.

---
+++ ex20/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int difference = Math.Abs(num1 - num2);
+
+            if (num1 > num2)
+            {
+                difference *= 2;
+            }
+
+            Console.WriteLine("Result: " + difference);
+        }
+    }
+}
